673efdabc402706c55b4c7303317de2420cebe8a
Luciano Bessegato <lfrbessegato@gmail.com>
2019-09-23 09:57:10 -0300

    Mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -12,6 +14,7 @@ public class ViewController {
 	@FXML
 	public void onBtTestAction() {
 		System.out.println("Click");
+		Alerts.showAlert("Alert Title", "Alert Header", "Hello", AlertType.INFORMATION);
 	}
 
 }
